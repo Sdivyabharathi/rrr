953b83b019a7889d879d44bb01d4020d7faa110e
Sdivyabharathi <divyaarul96@gmail.com>
2017-07-05 15:04:04 +0530

    Create True.java

---
+++ True.java
@@ -0,0 +1,19 @@
+# rrr
+import java.io.*;
+import java.util.*;
+public class True
+{
+  public static void main(String args[])throws IOException
+  {
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    String a="true";
+    String b="false";
+    System.out.println("Enter a day");
+    String day=br.readLine();
+    if(day.equals("sunday"))
+    System.out.println(b);
+    else 
+    System.out.println(a);
+   
+  }
+}
